3b2b9524a61db0adaae9cb0f7109201e6aa6cad2
ashok1230 <gadiashok0410@gmail.com>
2013-03-24 22:50:44 +0530

    first commit

---
+++ argument_passing.py
@@ -0,0 +1,15 @@
+class exe13:
+    def pc(self):
+    	from sys import argv
+    	self.script,self.first,self.second,self.third=argv
+class main(exe13):
+    def method(self):
+	exe13.pc(self)
+	print 'Script name is ',self.script
+	print 'First argumrent is ',self.first
+	print 'second argument is ',self.second
+	print 'Third argument is ',self.third
+a=main()
+a.method()
+
+	
